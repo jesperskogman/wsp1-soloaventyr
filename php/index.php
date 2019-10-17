824440353e473fcb7ba8947fdc33bbfcd99351c1
jesperskogman <jesper.skogman@elev.ga.ntig.se>
2019-10-17 11:01:01 +0200

    initial push

---
+++ php/index.php
@@ -0,0 +1,26 @@
+<?php
+
+include 'include/db.php';
+$filteredId = 1;
+
+if(isset($_GET['id'])){
+    $filteredId =filter_input(INPUT_GET, 'id', FILTER_VALIDATE_INT);
+}
+
+$sth = $dbh->prepare('SELECT * FROM story
+                    WHERE id = :filteredId');
+$sth->bindParam(':filteredId', $filteredId);
+$sth->execute();
+$story = $sth->fetch(PDO::FETCH_ASSOC);
+
+$sth = $dbh->prepare('SELECT * FROM links
+                    WHERE story_id = :filteredId');
+$sth->bindParam(':filteredId', $filteredId);
+$sth->execute();
+$links = $sth->fetchAll(PDO::FETCH_ASSOC);
+
+$page_title = "Soloäventyr - Start";
+
+include 'views/index.php';
+
+?>
